7ca9b50932818b3dfaebbafa2f0d37b391453ca5
Konny Daugh <natalydiach@gmail.com>
2025-03-20 17:15:31 +0100

    Added menu to mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 import '../sass/style.scss';
+
+const burger = document.querySelector('.burger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+
+burger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
