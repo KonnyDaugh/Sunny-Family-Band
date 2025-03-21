be1d0edec86b2160061b080be80111f64ff4ae86
Konny Daugh <natalydiach@gmail.com>
2025-03-21 13:03:27 +0100

    Added audio to playlist and youtube links to the video-gallery

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,33 @@ closeElem.addEventListener('click', () => {
 menuAbout.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+let currentAudio = null;
+
+document.querySelectorAll('.audio-player').forEach(player => {
+    const audio = player.querySelector('audio');
+    const playIcon = player.querySelector('.playIcon');
+    const pauseIcon = player.querySelector('.pauseIcon');
+
+function togglePlay() {
+    if (currentAudio && currentAudio !== audio) {
+        currentAudio.pause(); // Останавливаем другой играющий аудиофайл
+        currentAudio.closest('.audio-player').querySelector('.playIcon').style.display = "block";
+        currentAudio.closest('.audio-player').querySelector('.pauseIcon').style.display = "none";
+    }
+    if (audio.paused) {
+        audio.play();
+        playIcon.style.display = "none";
+        pauseIcon.style.display = "block";
+        currentAudio = audio; // Запоминаем текущий аудиофайл
+    } else {
+        audio.pause();
+        playIcon.style.display = "block";
+        pauseIcon.style.display = "none";
+        currentAudio = null;
+        }
+    }
+
+    playIcon.addEventListener("click", togglePlay);
+    pauseIcon.addEventListener("click", togglePlay);
+});
