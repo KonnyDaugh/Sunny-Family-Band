48feacac9f2f3001d86eef86caf64118fc972351
Konny Daugh <natalydiach@gmail.com>
2025-03-22 16:04:18 +0100

    Added base to vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,12 @@ import { defineConfig } from 'vite'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
+    base: '/Sunny-Family-Band/',
+    build: {
+        rollupOptions: {
+        input: {
+            main: resolve(__dirname, 'index.html'),
+        },
     },
   },
 })
